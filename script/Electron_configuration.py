83936f2ea3610ed9817c20521bc0b122ff6b0444
hzhou23 <hzhou23@ncsu.edu>
2022-08-14 15:56:22 -0400

    Minor fix to the head of pp.d

--- script/Electron_configuration.py
+++ script/Electron_configuration.py
@@ -148,7 +148,10 @@ class Electron_configure:
 
         for file_num, single_file in enumerate(path_file_name):
             with open(single_file, 'w') as f:
-                head = 'i\n{} 2000\nd\n1\nu\n0\na\n0 {} .5 .0001 100\n'.format(self.num_e, occupation_line[file_num])
+                if ae:
+                    head = 'i\n{} 2000\nd\n1\nu\n0\na\n0 {} .5 .0001 100\n'.format(self.num_e, occupation_line[file_num])
+                else:
+                    head = 'i\n{} 2000\nd\n0\nu\n2\ng\na\n0 {} .5 .0001 200\n'.format(self.num_e, occupation_line[file_num])
                 content = occupation_table[file_num]
                 tail = 'w\nout\nq'
                 combined_content = head+content+tail
