a752ca951a3c539361dbcb8de09756e240a69e04
hzhou23 <hzhou23@ncsu.edu>
2022-08-17 23:09:44 -0400

    Adding molpro states.proc functionality

--- script/Electron_configuration.py
+++ script/Electron_configuration.py
@@ -3,7 +3,40 @@
 import numpy as np
 import pandas as pd
 import os
+import ujson
 from typing import Dict, List, Tuple
+from itertools import combinations
+from collections import Counter
+from fractions import gcd
+from functools import reduce
+
+class orbital_sym():
+    def __init__(self, name: str, half_fill : int, full_fill : int, symmetries: List[int])-> None:
+        self.name = name
+        self.half_fill = half_fill
+        self.full_fill = full_fill
+        self.symmetries = symmetries
+    
+class s(orbital_sym):
+    
+    def __init__(self)-> None:
+        super().__init__( 's',1,2,[1])
+
+class p(orbital_sym):
+
+    def __init__(self)-> None:
+        super().__init__('p',3,6,[2,3,5])
+
+class d(orbital_sym):
+
+    def __init__(self)-> None:
+        super().__init__( 'd',5,10,[1,1,4,6,7])
+
+class f(orbital_sym):
+
+    def __init__(self)-> None:
+        super().__init__( 'f',7,14,[2,2,3,3,5,5,8])
+
 
 class orbital_occupation:
     def __init__(self, occ_list: List[List[int]]) -> None:
@@ -116,6 +149,182 @@ class orbital_occupation:
         
         return final_occ
 
+    def molpro_rhf(self, proc_name : None, state_average_orbs : List[str]) -> List[str]:
+        if proc_name == None:
+            
+            occ_list_rhf = []
+            for i_num, i in enumerate(self.occ_list):
+                state_weights, wf_triples = self.molpro_wf(i, state_average_orbs[i_num])
+                wf_triple = wf_triples[0]
+                molpro_occ_line, molpro_closed_line = self.molpro_occ_closed_line(i)
+
+                tmp_rhf = '{{rhf,nitord=1,maxit=0\n start,5202.2\n wf,{}\n occ,{}\n closed,{}\n}}\nendproc\n\n\n'.format(wf_triple, molpro_occ_line, molpro_closed_line)
+                occ_list_rhf.append(tmp_rhf)
+
+        else:
+
+            occ_list_rhf = []
+            for i_num, i in enumerate(self.occ_list):
+                state_weights, wf_triples = self.molpro_wf(i, state_average_orbs[i_num])
+                wf_triple = wf_triples[0]
+                molpro_occ_line, molpro_closed_line = self.molpro_occ_closed_line(i)
+
+                tmp_rhf = 'proc {}\n{{rhf\n start,atden\n print,orbitals=2\n wf,{}\n occ,{}\n closed,{}\n orbital,4202.2\n}}'.format(proc_name[i_num], wf_triple, molpro_occ_line, molpro_closed_line)
+                occ_list_rhf.append(tmp_rhf)
+
+        return occ_list_rhf
+
+
+    def molpro_multi(self, state_average_orbs : List[str]) -> List[str]:
+        multi_list = []
+
+        for i_num,i in enumerate(self.occ_list):
+            pseudo_occ_state, pseudo_closed_state = self.molpro_pseudo(i, state_average_orb = state_average_orbs[i_num])
+            pseudo_occ_line = self.molpro_occ_closed_line(pseudo_occ_state)[0]
+            pseudo_closed_line = self.molpro_occ_closed_line(pseudo_closed_state)[1]
+            state_weights,wf_triples = self.molpro_wf(i, state_average_orb = state_average_orbs[i_num])
+            multi_content = '\npop\n{{multi\n start,4202.2\n occ,{}\n closed,{}\n'.format(pseudo_occ_line, pseudo_closed_line)
+            for i_num in range(len(wf_triples)):
+                multi_content += ' wf,{};state,{}\n'.format(wf_triples[i_num],state_weights[i_num])
+            multi_content += ' orbital,5202.2\n}\n'
+            
+            multi_list.append(multi_content)
+
+        return multi_list
+
+
+
+    def molpro_wf(self, state : List[int], state_average_orb: str) -> Tuple[List[int],List[str]]: 
+        ''' For a given input state, and the given state average orbital, it will return : number of valence electron, total symmetry and number of open shell electron as a list.'''
+        
+        weights=[]
+        molpro_wf_list=[]
+        for k,v in self.orbital_map.items():
+            if v == state_average_orb:
+                num_e = state[k]
+        
+        if state_average_orb[1] == 's':
+            orb_feature = s()
+        elif state_average_orb[1] == 'p':
+            orb_feature = p()
+        elif state_average_orb[1] == 'd':
+            orb_feature = d()
+        elif state_average_orb[1] == 'f':
+            orb_feature = f()
+
+        site=list(range(len(orb_feature.symmetries)))
+        if num_e <= orb_feature.half_fill:
+            open_shell = num_e
+        else:
+            open_shell = orb_feature.full_fill - num_e
+
+        index_list = list(combinations(site, num_e))
+        if len(index_list) == 0:
+            index_list = [(0,0)]
+        symmetry = []
+        for ind in index_list :
+            tmp_sym = [orb_feature.symmetries[i] for i in ind]
+            symmetry.append(total_sym(tmp_sym))
+        
+        sym_counter = Counter(symmetry)
+        for k,v in sym_counter.items():
+            wf = [0,0,0]
+            wf[0] = sum(state)
+            wf[1] = int(k)
+            wf[2] = open_shell
+            weights.append(v)
+            wf = ','.join(map(str, wf))
+            molpro_wf_list.append(wf)
+        
+        weights_gcd = reduce(gcd, weights)
+        state_weights = [int(i/weights_gcd) for i in weights]
+        return state_weights,molpro_wf_list
+
+    
+
+    def molpro_occ_closed_line(self, state:List[int])->Tuple[str,str]:
+        occ_line = [0]*8
+        closed = [0]*8
+        for i_num, i in enumerate(state):
+            orb = self.orbital_map[i_num][1]
+            tmp_occ_line,tmp_closed = self.molpro_single_orb_sym_occ_closed(orb=orb, occ = i)
+            occ_line = [occ_line[i] + tmp_occ_line[i] for i in range(8)]
+            closed = [closed[i] + tmp_closed[i] for i in range(8)]
+        occ_line=','.join(map(str, occ_line))
+        closed = ','.join(map(str, closed))
+        return occ_line, closed
+
+    
+
+    
+
+    def molpro_pseudo(self, state: List[int], state_average_orb : str) -> Tuple[List[int], List[int]]:
+        
+        pseudo_occ_state=ujson.loads(ujson.dumps(state))
+        pseudo_closed_state=ujson.loads(ujson.dumps(state))
+        
+        for k,v in self.orbital_map.items():
+            if v == state_average_orb:
+                pseudo_closed_state[k] = 0
+                if v[1] == 's':
+                    pseudo_occ_state[k] = 2
+                elif v[1] == 'p':
+                    pseudo_occ_state[k] = 6
+                elif v[1] == 'd':
+                    pseudo_occ_state[k] = 10
+                elif v[1] == 'f':
+                    pseudo_occ_state[k] = 14
+                else:
+                    raise Exception("Only s, p, d, f orbitals supported in state average calculations.")
+                    
+        return pseudo_occ_state, pseudo_closed_state
+
+    def molpro_single_orb_sym_occ_closed(self, orb : str, occ: int) ->Tuple[ List[int], List[int]]:
+        sym_occ_list = [0]*8
+        sym_closed_list = [0]*8
+        if orb == 's':
+            orb_f = s()
+            occ_list = [0]
+            closed_list = [0]
+            occ_list[0] = occ
+            for i_num,i in enumerate(occ_list):
+                if i != 0:
+                    sym_occ_list[orb_f.symmetries[i_num]-1] += 1
+                if i == 2:
+                    sym_closed_list[orb_f.symmetries[i_num]-1] += 1
+
+        elif orb == 'p':
+            orb_f = p()
+            base,extra = divmod(occ, 3)
+            occ_list = [base+(i<extra) for i in range(3)]
+            for i_num,i in enumerate(occ_list):
+
+                if i != 0:
+                    sym_occ_list[orb_f.symmetries[i_num]-1] += 1
+                if i == 2:
+                    sym_closed_list[orb_f.symmetries[i_num]-1] += 1
+
+        elif orb =='d' :
+            orb_f = d()
+            base,extra = divmod(occ, 5)
+            occ_list = [base+(i<extra) for i in range(5)]
+            for i_num,i in enumerate(occ_list):
+                if i != 0:
+                    sym_occ_list[orb_f.symmetries[i_num]-1] += 1
+                if i == 2:
+                    sym_closed_list[orb_f.symmetries[i_num]-1] += 1
+
+        elif orb =='f' :
+            orb_f = f()
+            base,extra = divmod(occ, 7)
+            occ_list = [base+(i<extra) for i in range(7)]
+            for i_num,i in enumerate(occ_list):
+                if i != 0:
+                    sym_occ_list[orb_f.symmetries[i_num]-1] += 1
+                if i == 2:
+                    sym_closed_list[orb_f.symmetries[i_num]-1] += 1
+        
+        return sym_occ_list,sym_closed_list
 
 class Electron_configure:
     def __init__(self, name : str, num_e : int, core: List[int], ecp_states: List[List]) -> None:
@@ -166,9 +375,55 @@ class Electron_configure:
                 combined_content = head+content+tail
                 f.write(combined_content)
 
-    def write_Molpro(self, molpro_path : str, ae : bool = True) -> None:
+    def write_Molpro(self, molpro_path : str , proc_name: List[str], state_average_orbs: List[str] ,ae = True) -> None:
+        assert len(proc_name) == len(self.ae_states), "Total number of states in states.proc should be the same of the number of given states."
+        print("Writing Molpro files...\n NOTE: In the states.proc file, no 'sym' or 'restrict' were added, please make sure you add them later on.")
+        os.makedirs(molpro_path, exist_ok = True)
+        if ae:
+            statesproc = molpro_path + '/states_ae_nosym.proc'
+            first_rhf = orbital_occupation(occ_list = self.ae_states).molpro_rhf(proc_name = proc_name, state_average_orbs = state_average_orbs)
+            multi = orbital_occupation(occ_list = self.ae_states).molpro_multi(state_average_orbs = state_average_orbs)
+            last_rhf = orbital_occupation(occ_list = self.ae_states).molpro_rhf(proc_name = None,state_average_orbs = state_average_orbs)
+        else :
+            statesproc = molpro_path + '/states_ecp_nosym.proc'
+            first_rhf = orbital_occupation(occ_list = self.ecp_states).molpro_rhf(proc_name = proc_name, state_average_orbs = state_average_orbs)
+            multi = orbital_occupation(occ_list = self.ecp_states).molpro_multi(state_average_orbs = state_average_orbs)
+            last_rhf = orbital_occupation(occ_list = self.ecp_states).molpro_rhf(proc_name = None,state_average_orbs = state_average_orbs)
+        
+        final_content = ""
+        for i_num in range(len(first_rhf)):
+            final_content += first_rhf[i_num]+multi[i_num]+last_rhf[i_num]
+        
+        with open(statesproc, 'w') as f:
+            f.write(final_content)
 
 
+def bipart_sym(sym_1 : int, sym_2 : int) -> int:
+    sym_dict = {(1,1): 1, (1,2):2, (1,3): 3, (1,4):4, (1,5):5, (1,6):6, (1,7):7, (1,8):8,
+            (2,2):1, (2,3):4, (2,4):3, (2,5):6, (2,6):5, (2,7):8, (2,8):7,
+            (3,3):1, (3,4):2, (3,5):7, (3,6):8, (3,7):5, (3,8):6,
+            (4,4):1, (4,5):8, (4,6):7, (4,7):6, (4,8):5,
+            (5,5):1, (5,6):2, (5,7):3, (5,8):4,
+            (6,6):1, (6,7):4, (6,8):3,
+            (7,7):1, (7,8):2,
+            (8,8):1}
 
+    for k,v in sym_dict.items():
+        if sym_1 <= sym_2:
+            if sym_1 == k[0] and sym_2 == k[1]:
+                return v
+        else :
+            if sym_2 == k[0] and sym_1 == k[1]:
+                return v
+
+
+def total_sym(sym_list: List[int])-> int:
+    assert len(sym_list) >= 1, "symmetry list cannot be smaller than 1."
+
+    current_sym = 1 
+    for i_num in range(len(sym_list)):
+        current_sym = bipart_sym(current_sym, sym_list[i_num])
+
+    return current_sym
 
 
--- script/Y.py
+++ script/Y.py
@@ -7,7 +7,7 @@ from Electron_configuration import *
 name = 'Y'
 num_e = 39
 core = [2,2,6,2,6,10]
-ecp_states = [[0,0,0,0,0,0], [0,0,0,0,0,0,2,6,1,0,2], [0,0,0,0,0,0,2,6,0,0,2,1], [0,0,0,0,0,0,2,6,2,0,2], [0,0,0,0,0,0,2,6,0,0,2], [0,0,0,0,0,0,2,6,1,0,1], [0,0,0,0,0,0,2,6,1], [0,0,0,0,0,0,2,6],[0,0,0,0,0,0,2,5],[0,0,0,0,0,0,2,4],[0,0,0,0,0,0,2,3],[0,0,0,0,0,0,2,2],[0,0,0,0,0,0,2,1],[0,0,0,0,0,0,2]]
+ecp_states = [ [0,0,0,0,0,0,2,6,1,0,2], [0,0,0,0,0,0,2,6,0,0,2,1], [0,0,0,0,0,0,2,6,2,0,2], [0,0,0,0,0,0,2,6,0,0,2], [0,0,0,0,0,0,2,6,1,0,1], [0,0,0,0,0,0,2,6,1], [0,0,0,0,0,0,2,6],[0,0,0,0,0,0,2,5],[0,0,0,0,0,0,2,4],[0,0,0,0,0,0,2,3],[0,0,0,0,0,0,2,2],[0,0,0,0,0,0,2,1],[0,0,0,0,0,0,2]]
 
 Y = Electron_configure(name = name, num_e = num_e, core = core, ecp_states = ecp_states)
 #print(Y.ae_configures)
@@ -15,10 +15,18 @@ Y = Electron_configure(name = name, num_e = num_e, core = core, ecp_states = ecp
 
 work_path = os.getcwd()
 result_path_ae = work_path+'/result/AE/'
-ae_file_names = ["ae0.d","ae1.d","ae2.d","ae3.d","ae4.d","ae5.d","ae6.d","ae7.d","ae8.d","ae9.d","ae10.d","ae11.d","ae12.d","ae13.d"]
+ae_file_names = ["ae1.d","ae2.d","ae3.d","ae4.d","ae5.d","ae6.d","ae7.d","ae8.d","ae9.d","ae10.d","ae11.d","ae12.d","ae13.d"]
 Y.write_yoon(result_path = result_path_ae, file_names = ae_file_names, ae = True)
 
 result_path_ecp = work_path + '/result/ECP/'
-ecp_file_names = ["pp0.d","pp1.d","pp2.d","pp3.d","pp4.d","pp5.d","pp6.d","pp7.d","pp8.d","pp9.d","pp10.d","pp11.d","pp12.d","pp13.d"]
+ecp_file_names = ["pp1.d","pp2.d","pp3.d","pp4.d","pp5.d","pp6.d","pp7.d","pp8.d","pp9.d","pp10.d","pp11.d","pp12.d","pp13.d"]
 Y.write_yoon(result_path = result_path_ecp, file_names = ecp_file_names, ae = False)
 
+
+molpro_path = './'
+proc_names = [ "Id1s2", 'Is2p1','EAd2s2','IPds2','IId1s1','IIId1','IVp6','Vp5','VIp4','VIIp3','VIIIp2','IXp1','Xp' ]
+S_A =[ "4d", "5p", "4d", "5s", "4d", "4d", "4p", "4p", "4p", "4p","4p","4p","4s"]
+Y.write_Molpro(molpro_path = molpro_path, proc_name = proc_names, state_average_orbs = S_A, ae = True)
+
+molpro_path = './'
+Y.write_Molpro(molpro_path = molpro_path, proc_name = proc_names, state_average_orbs = S_A, ae = False)
