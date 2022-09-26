70cc83738111ef8f38be575fd8cba51c3fbb3a92
hzhou23 <hzhou23@ncsu.edu>
2022-09-25 19:52:53 -0700

    Modifying RhO

--- Rh/Molpro/molecules/RhO/tz.sh
+++ Rh/Molpro/molecules/RhO/tz.sh
@@ -9,7 +9,7 @@ do
 echo $j
 rm $HOME/$j/tzbind
 echo r bind > $HOME/$j/tzbind
-for i in $(seq 1.10 0.10 2.60 )
+for i in $(seq 1.30 0.10 2.20 )
 do
 cd $HOME/$j/r_$i
 Ebind=$(awk '{print $4}' tz.table1.txt)
