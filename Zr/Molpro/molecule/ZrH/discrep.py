dc72eeaa52031c08e9f6193ed29d03bda2d92d6e
hzhou23 <hzhou23@ncsu.edu>
2022-09-27 12:25:53 -0700

    ZrH modified

--- Zr/Molpro/molecule/ZrH/discrep.py
+++ Zr/Molpro/molecule/ZrH/discrep.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 toev=27.21138602
 
-ecps = ['UC', 'crenbl', 'lanl2', 'mdfstu', 'mwbstu','sbkjc']#, 'w3', 'w6', 'w9']#,'i0','i7', 'i6']#'sub0','smal-se3','se3','se4']
+ecps = ['UC', 'crenbl', 'lanl2', 'mdfstu', 'mwbstu','sbkjc','ECP2']#, 'w3', 'w6', 'w9']#,'i0','i7', 'i6']#'sub0','smal-se3','se3','se4']
 styles = {
 'UC'        :{'label': 'UC',       'color':'#8B3E2F','linestyle':'-'},
 'crenbl'    :{'label': 'CRENBL','color':'#ff7f00','linestyle':'--','dashes': (8,5,1,3)},
@@ -24,7 +24,7 @@ styles = {
 #'smal-se3'     :{'label': 'energy3',      'color':'#006666','linestyle':'--','dashes': (4,3)     },
 #
 'mwbstu'     :{'label': 'MWBSTU',      'color':'#008000','linestyle':'--','dashes': (6,6)     },
-#'ECP'      :{'label': 'ccECP',    'color':'#e41a1c','linestyle':'--','dashes': (4,2,1,2) },
+'ECP2'      :{'label': 'ECP2',    'color':'#e41a1c','linestyle':'--','dashes': (4,2,1,2) },
 #'ECP1'      :{'label': 'ECP1.1',    'color':'#cc0099','linestyle':'--','dashes': (4,2,1,2) },
 #'ECP1_2'      :{'label': 'ECP1.2',    'color':'#39e600','linestyle':'--','dashes': (4,2,1,2) },
 #'w3'     :{'label': 'w3',      'color':'#4daf4a','linestyle':'-','dashes': (2,3)     },
