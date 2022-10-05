9c1147b82c0807d2e2e828b045be95675a368d33
hzhou23 <hzhou23@ncsu.edu>
2022-10-04 21:19:12 -0700

    YO range

--- Y/Molpro/molecule/YO/discrep.py
+++ Y/Molpro/molecule/YO/discrep.py
@@ -74,7 +74,7 @@ def plot():
         else:
             y = (data[ecp] - data['ae']+0.01049448)*toev
         plt.plot(x,y,**styles[ecp])
-    ax.set_xlim((1.20,2.30))
+    ax.set_xlim((1.30,2.30))
 #    ax.set_ylim((-0.25,0.25))
     ax.set(title='YO tz Discrepancies')
     #plt.legend(bbox_to_anchor=(0.53, 0.15, 0.5, 0.5), fontsize="x-small")
