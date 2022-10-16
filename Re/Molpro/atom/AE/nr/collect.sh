522fbb3cdf8eac3f9a6384020f4297f14a72c5fd
hzhou23 <hzhou23@ncsu.edu>
2022-10-15 19:02:14 -0700

    Re nr test run

---
+++ Re/Molpro/atom/AE/nr/collect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOME=`pwd`
+
+n=16
+
+rm $HOME/tz.table1.csv
+
+echo 'SCF' > $HOME/tz.table1.csv
+
+for ((i=1;i<=n;i++))
+do
+	cd $HOME/$i
+	echo $i
+	grep '-' tz.table1.csv | awk '{print $1}' >> $HOME/tz.table1.csv
+done
