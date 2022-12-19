dc440e6bcb23a30392eb8f658886fe9449c17a4e
hzhou23 <hzhou23@ncsu.edu>
2022-12-18 20:14:56 -0800

    Gd ECP13

--- Gd/Molpro/molecule/GdO/discrep.py
+++ Gd/Molpro/molecule/GdO/discrep.py
@@ -81,7 +81,7 @@ def plot():
         y= (data[ecp] - data['ae'])*toev
         plt.plot(x,y,**styles[ecp])
     ax.set_xlim((1.40,2.10))
-    #ax.set_ylim((-0.40,0.35))
+    ax.set_ylim((-0.30,0.35))
     ax.set(title='GdO tz Discrepancies')
     #plt.legend(bbox_to_anchor=(0.53, 0.15, 0.5, 0.5), fontsize="x-small")
     plt.legend(loc='best',ncol=2,prop={'size': 15})
