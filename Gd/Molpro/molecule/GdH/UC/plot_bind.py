4fd25fe286a221aec5d4b206d556f00dfe76f787
hzhou23 <hzhou23@ncsu.edu>
2022-12-28 17:01:29 -0800

    GdH ecp and uc

--- Gd/Molpro/molecule/GdH/UC/plot_bind.py
+++ Gd/Molpro/molecule/GdH/UC/plot_bind.py
@@ -15,14 +15,17 @@ toev = 27.211386
 
 
 binding = pd.read_csv("prebind", delim_whitespace=True)
-print (binding)
+ecpbinding = pd.read_csv("ECPprebind", delim_whitespace=True)
+print (binding, ecpbinding)
 #binding = binding.drop([0]).reset_index(drop=True)
 #print binding
 r=binding['r']
 bind = binding['bind']
+ecpbind = ecpbinding['bind']
 fig=plt.figure()
 ax=fig.add_subplot(111)
 
 plt.plot(r,bind)
+plt.plot(r,ecpbind,color = 'r')
 plt.savefig('prebind.pdf')
 
